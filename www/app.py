9e51ee375cf0d01b0d283dee370c1ecd43598a58
Van Huang <huangvan0808@gmail.com>
2018-08-04 12:55:23 +0800

    complete web app framwork

--- www/app.py
+++ www/app.py
@@ -5,3 +5,26 @@ Created on Fri Aug  3 23:05:53 2018
 @author: Van Huang
 """
 
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from datetime import datetime
+from aiohttp import web
+
+
+def index(request):
+ #   yield from asyncio.sleep(0.5)
+    return web.Response(body = b'<h1>WEBServer DEMO</h1>', content_type='text/html')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    app.router.add_route('POST', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('web server started at http://127.0.0.1:9000...')
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
